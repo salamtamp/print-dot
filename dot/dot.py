b3f97f65aa8c95c076ad8e49d2219a0b1b072799
salamtamp <salamtam.p@gmail.com>
2020-09-10 20:51:24 +0700

    update code

---
+++ dot/dot.py
@@ -0,0 +1,17 @@
+import time
+
+
+class Dot:
+    def __init__(self):
+        pass
+
+    def print_one():
+        print(".")
+
+    def print_two():
+        print("..")
+
+    def print_every_1_sec():
+        while True:
+            print(".")
+            time.sleep(1)
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="print-dot-salamtam",
+    version="0.0.1",
+    author="salamtam",
+    author_email="salamtam.p@gmail.com",
+    description="print dot for test the coding work",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/salamtamp/print-dot",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
